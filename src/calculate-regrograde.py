3fae217a2bd6de56acfe8c746e7a99fe8e2178e3
Alan Smith <commit@alanwsmith.com>
2022-12-08 19:34:44 -0500

    moved to new fiel

--- src/calculate-regrograde.py
+++ src/calculate-regrograde.py
@@ -1,69 +1,41 @@
 #!/usr/bin/env python3
 
-import calendar
-import datetime 
+
+import datetime
 import json
-import re
-import time
+import numpy as np 
+import matplotlib.pyplot as plt
 
 from skyfield.api import load
 
-def check_retro(planet, t1):
+planets = load('de421.bsp')
+earth = planets['earth']
+scale = load.timescale(builtin=True)
 
-    # Fetch data from the United States Naval Observatory 
-    # and the International Earth Rotation Service
-    planets = load('de440s.bsp')
+def is_retro(target_planet, date):
+    target = planets[target_planet]
 
-    # Setup earth and target planet 
-    earth, target_planet = planets['earth'], planets[planet]
+    time_alfa = scale.utc(date)
+    time_bravo = scale.utc(date + datetime.timedelta(minutes=5))
 
-    # Load a timescale so that we can translate 
-    # between different systems for expressing time
-    ts = load.timescale(builtin=True)
+    lat_alfa, lon_alfa, dist_alfa = earth.at(time_alfa).observe(target).ecliptic_latlon()
+    lat_bravo, lon_bravo, dist_bravo = earth.at(time_bravo).observe(target).ecliptic_latlon()
 
-    # Create a new time with a delta for comparison
-    t2 = t1 + datetime.timedelta(minutes=5)
+    num_alfa = (180./np.pi) * lon_alfa.radians
+    num_bravo = (180./np.pi) * lon_bravo.radians
 
-    # Move to terrestrial times
-    ttime1 = ts.utc(t1.year, t1.month, t1.day, t1.hour, t1.minute)
-    ttime2 = ts.utc(t2.year, t2.month, t2.day, t2.hour, t2.minute)
-
-    # Get atrometric measurements 
-    astrometric1 = earth.at(ttime1).observe(target_planet)
-    astrometric2 = earth.at(ttime2).observe(target_planet)
-
-    # Load: right ascension (ra),
-    # decline (dec), and distance (dist)
-    ra1, dec1, dist1 = astrometric1.radec()
-    ra2, dec2, dist2 = astrometric2.radec()
-
-    # Split arrays to pull only the numeric values
-    arr1 = re.sub(r'[a-zA-Z]+', '', str(ra1)).split()
-    arr2 = re.sub(r'[a-zA-Z]+', '', str(ra2)).split()
-
-    ftr = [3600,60,1]
-
-    # Get Angle outputs in seconds
-    time1_seconds = sum([a*b for a,b in zip(ftr, map(float,arr1))])
-    time2_seconds = sum([a*b for a,b in zip(ftr, map(float,arr2))])
-
-    # Find difference in RAs between times
-    ra_diff = float(time2_seconds - time1_seconds)
-
-    # Interpret output of differences in RAs
-    if ra_diff < 0.000000:
-        # In retrograde
-    	is_retro = True
-    elif ra_diff > 0.000000:
-        # Not in retrograte
-    	is_retro = False 
+    if num_alfa < num_bravo:
+        return False
     else:
-        # Not aligned (leaving this for possible 
-        # third display value that was used in source)
-        is_retro = False 
-    return is_retro 
+        return True
 
-planets = [
+if __name__ == "__main__":
+
+    data = {
+        "dates": {}
+    }
+
+    planet_list = [
         (1, "mercury"), 
         (2, "venus"), 
         (4, "mars"), 
@@ -75,27 +47,14 @@ planets = [
         (301, "moon") 
     ]
 
-data = {"dates": {}}
-
-for year in range(2000, 2001):
-    print(f"Calculating year: {year}")
-    start_date  = datetime.datetime(year, 1, 1, 0, 0, 0, 0, datetime.timezone.utc)
-    days = 365
-    if calendar.isleap(year):
-        days = 366
-    for offset in range(0, days):
-        d = start_date + datetime.timedelta(offset)
+    start_date  = datetime.datetime(2022, 1, 1, 0, 0, 0, 0, datetime.timezone.utc)
+    for i in range(0, 365):
+        date = start_date + datetime.timedelta(i)
         details = {}
+        for planet in planet_list:
+            details[planet[1]] = is_retro(planet[0], date)
+        data["dates"][date.strftime("%Y-%m-%d")] = details
 
-        for planet in planets:
-            details[planet[1]] = check_retro(planet[0], d)
-        data["dates"][d.strftime("%Y-%m-%d")] = details
-
-        # # TMP
-        # tmp_value = check_retro(1, d)
-        # print(f"{d.year}-{d.month}-{d.day} {tmp_value}")
-
-
-with open('retrograte.json', 'w') as _out:
-    json.dump(data, _out, sort_keys=True, indent=2)
+    with open("retrogrades.json", "w") as _out:
+        json.dump(data, _out, sort_keys=True, indent=2)
 
